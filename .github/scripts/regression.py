7fc3a3d268f6c48aa91a0bab77fd704b7506ca73
hjjq <hanjie.qiu@mail.utoronto.ca>
2023-11-20 15:51:32 -0500

    .

--- .github/scripts/regression.py
+++ .github/scripts/regression.py
@@ -4,6 +4,7 @@ import mysql.connector
 
 # e.g., '1,2,3'
 hw_config_ids = os.environ.get('HW_CONFIG')
+hw_config_ids = hw_config_ids.split(',')
 
 conn = mysql.connector.connect(
     host=os.environ.get('CI_DB_HOSTNAME'),
@@ -12,13 +13,18 @@ conn = mysql.connector.connect(
     port=os.environ.get('CI_DB_PORT'),
     database='hidet_ci'
 )
-
 cursor = conn.cursor()
-query = f"SELECT cloud_provider_id, instance_id FROM cloud_instance WHERE hardware_config_id IN ({hw_config_ids})"
-cursor.execute(query)
 
-for row in cursor:
-    print(row)
+for hw_config_id in hw_config_ids:
+    query = (
+        'SELECT cloud_provider_id, instance_id FROM cloud_instance '
+        f'WHERE hardware_config_id = {hw_config_id} LIMIT 1'
+    )
+    cursor.execute(query)
+    if cursor.rowcount == 0:
+        raise ValueError(f'Instance with hardware config id {hw_config_id} does not exist.')
+    for row in cursor:
+        print(row)
 
 cursor.close()
 conn.close()
