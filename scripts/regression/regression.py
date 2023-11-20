3f322e979873250cb9770231bc2e46ef66ae89ba
hjjq <hanjie.qiu@mail.utoronto.ca>
2023-11-19 23:03:04 -0500

    .

--- scripts/regression/regression.py
+++ scripts/regression/regression.py
@@ -1,3 +1,22 @@
 import os
 
-print("regression.py:" + os.environ.get('DB_PASSWORD'))
+
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host=os.environ.get('CI_DB_HOSTNAME'),
+    user=os.environ.get('CI_DB_USERNAME'),
+    password=os.environ.get('CI_DB_PASSWORD'),
+    port=os.environ.get('CI_DB_PORT'),
+    database='hidet_ci'
+)
+
+cursor = conn.cursor()
+query = "SELECT * FROM hardware_config"
+cursor.execute(query)
+
+for row in cursor:
+    print(row)
+
+cursor.close()
+conn.close()
