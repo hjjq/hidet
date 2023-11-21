8dfc5f5983148dbf436376c6ec8d9754c20a7c03
hjjq <hanjie.qiu@mail.utoronto.ca>
2023-11-21 17:05:32 -0500

    .

--- .github/scripts/start_instances.py
+++ .github/scripts/start_instances.py
@@ -27,7 +27,8 @@ instances = []
 # Fetch list of (cloud_provider_id, instance_id) tuples from DB
 for hw_config_id in hw_config_ids:
     query = (
-        'SELECT cloud_provider_id, instance_id FROM cloud_instance '
+        'SELECT cloud_provider_id, instance_id, hardware_config.name as hw_config, FROM cloud_instance '
+        'JOIN hardware_config ON cloud_instance.hardware_config_id = hardware_config.id '
         f'WHERE hardware_config_id = {hw_config_id} LIMIT 1'
     )
     cursor.execute(query)
@@ -42,7 +43,7 @@ conn.close()
 
 # Start all instances
 for instance in instances:
-    cloud_provider_id, instance_id = instance
+    cloud_provider_id, instance_id, _ = instance
     if cloud_provider_id == 1: # AWS
         cmd = ['aws', 'ec2', 'start-instances', '--instance-ids', instance_id]
     else:
@@ -53,7 +54,7 @@ for instance in instances:
 
 # Wait until all instances are running
 for instance in instances:
-    cloud_provider_id, instance_id = instance
+    cloud_provider_id, instance_id, _ = instance
     started = False
     while not started:
         time.sleep(5)
@@ -67,7 +68,9 @@ for instance in instances:
         else:
             raise ValueError(f'Unknown cloud provider id: {cloud_provider_id}')
 
-# Set output for github action
+# Set outputs for subsequent jobs to use
+
+# String representing launched instances
 # e.g., "1,aws-instance0;1,aws-instance1;2,gcp-instance0" representing two AWS instances and one GCP instance
 instances_str = ''
 for instance in instances:
@@ -75,3 +78,17 @@ for instance in instances:
     instances_str += f'{cloud_provider_id},{instance_id};'
 with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
     print(f'started_instances={instances_str}', file=fh)
+
+# String representing JSON Array of hardware configs of the runners
+# e.g., "[\"A10\", \"A100\", \"RTX4090\"]"
+hw_config_json = '['
+for instance in instances:
+    _, _, hw_config = instance
+    s = '\\"'
+    s += hw_config
+    s += '\\",'
+    hw_config_json += s
+# Replace trailing comma with closing bracket
+hw_config_json[-1] = ']'
+with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+    print(f'hw_configs={hw_config_json}', file=fh)
