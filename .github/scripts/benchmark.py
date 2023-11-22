a9fd414485f859fe0af4757097a23a7a96d457a5
hjjq <hanjie.qiu@mail.utoronto.ca>
2023-11-22 13:01:35 -0500

    .

--- .github/scripts/benchmark.py
+++ .github/scripts/benchmark.py
@@ -1,5 +1,7 @@
 import torch
 import hidet
+import json
+import mysql.connector
 
 a = hidet.randn([1, 3, 224, 224], dtype='float16', device='cuda')
 b = hidet.randn([1, 1, 224, 224], dtype='float16', device='cuda')
