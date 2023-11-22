918abec048ad6032c4666ba1cf85612de19c7450
hjjq <hanjie.qiu@mail.utoronto.ca>
2023-11-22 14:55:37 -0500

    .

--- .github/scripts/upload_results.py
+++ .github/scripts/upload_results.py
@@ -1,6 +1,11 @@
-
+import os
 import json
-fh = open('run_configs.json')
-run_configs = json.load(fh)
-fh.close()
-print(run_configs)
+# fh = open('run_configs.json')
+# run_configs = json.load(fh)
+# fh.close()
+# print(run_configs)
+
+sha = os.environ.get('COMMIT_SHA')
+repo_name = os.environ.get('REPO_NAME')
+print(sha)
+print(repo_name)
