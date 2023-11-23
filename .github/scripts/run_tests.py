97f5f39c9e01aacf66b780267758c591c4226a83
hjjq <hanjie.qiu@mail.utoronto.ca>
2023-11-23 15:11:26 -0500

    .

--- .github/scripts/run_tests.py
+++ .github/scripts/run_tests.py
@@ -1,14 +1,7 @@
 import os
-import torch
-import hidet
 import json
 import mysql.connector
 import numpy as np
-
-a = hidet.randn([1, 3, 224, 224], dtype='float16', device='cuda')
-b = hidet.randn([1, 1, 224, 224], dtype='float16', device='cuda')
-c = a + b
-print(c)
 
 # fh = open('run_configs.json')
 # run_configs = json.load(fh)
@@ -29,7 +22,15 @@ print(c)
 # with open('run_configs.json', 'w') as fh:
 #     json.dump(run_configs, fh)
 
-def bench
+def get_bench_cmd(run_type, run_id, run_name, run_param_id, run_param_name):
+    cmd = ['python']
+    if run_name in ['bert-base-uncased', 'other models that share same interface']:
+        pass
+    elif run_name in ['another group of models/operators']:
+        pass
+
+    return cmd
 
 run_type, run_id, run_name, run_param_id, run_param_name = 'model', 1, 'bert-base-uncased', 1, 'seqlen=256'
 
+cmd = get_bench_cmd(run_type, run_id, run_name, run_param_id, run_param_name)
