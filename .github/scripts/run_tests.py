356902c9b27dcc2fe6af06f52111ab586c316dd0
hjjq <hanjie.qiu@mail.utoronto.ca>
2023-11-28 17:25:47 -0500

    .

--- .github/scripts/run_tests.py
+++ .github/scripts/run_tests.py
@@ -5,29 +5,11 @@ import mysql.connector
 import numpy as np
 import tqdm
 
-# fh = open('run_configs.json')
-# run_configs = json.load(fh)
-# fh.close()
-# print(run_configs)
-# hw_config = os.environ.get('HW_CONFIG')
-# print('hw:', hw_config)
-# for run_config in run_configs:
-#     # Append hardware_config column
-#     run_config['hardware_config'] = hw_config
-#     # Extract configurations
-#     run_type = run_config['type']
-#     run_id = run_config['id']
-#     run_name = run_config['name']
-#     run_param_id = run_config['param_id']
-#     run_param_name = run_config['param_name']
-#     run_config['latency'] = np.random.random() * 10
-# with open('run_configs.json', 'w') as fh:
-#     json.dump(run_configs, fh)
-
 def run_command(cmd):
     cmd = " ".join(cmd)
     print("Running command: " + cmd)
-    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+    # popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, shell=True)
     outputs = []
     for line in popen.stdout:
         print(line, end='')
@@ -59,9 +41,39 @@ def get_bench_cmd(run_type, run_id, run_name, run_param_id, run_param_name, dtyp
     cmd = ['python', runfile, run_name, '--params', run_param_name, '--dtype', dtype]
     return cmd
 
-run_type, run_id, run_name, run_param_id, run_param_name = 'model', 1, 'bert-base-uncased', 1, 'seqlen=256'
-dtype = 'float16'
-cmd = get_bench_cmd(run_type, run_id, run_name, run_param_id, run_param_name, dtype)
-outputs = run_command(cmd)
-latency = float(outputs[-1].split('\n')[0]) # Get last line
-print(f'Latency = {latency}')
+fh = open('run_configs.json')
+run_configs = json.load(fh)
+fh.close()
+print(run_configs)
+hw_config = os.environ.get('HW_CONFIG')
+print('hw:', hw_config)
+for run_config in run_configs:
+    # Append hardware_config column
+    run_config['hardware_config'] = hw_config
+    # Extract configurations
+    run_type = run_config['type']
+    run_id = run_config['id']
+    run_name = run_config['name']
+    run_param_id = run_config['param_id']
+    run_param_name = run_config['param_name']
+    cmd = get_bench_cmd(run_type, run_id, run_name, run_param_id, run_param_name, 'float16')
+    outputs = run_command(cmd)
+    latency = float(outputs[-1].split('\n')[0]) # Get last line
+    run_config['dtype_id'] = 1
+    run_config['dtype_name'] = 'float16'
+    run_config['latency'] = latency
+with open('run_configs.json', 'w') as fh:
+    json.dump(run_configs, fh)
+
+# run_type, run_id, run_name, run_param_id, run_param_name = 'model', 1, 'bert-base-uncased', 1, 'seqlen=256'
+# dtype = 'float16'
+# cmd = get_bench_cmd(run_type, run_id, run_name, run_param_id, run_param_name, dtype)
+# outputs = run_command(cmd)
+# latency = float(outputs[-1].split('\n')[0]) # Get last line
+# print(f'{run_name} Latency = {latency}')
+
+# run_type, run_id, run_name, run_param_id, run_param_name = 'model', 4, 'resnet50', 4, '1x3x224x224'
+# cmd = get_bench_cmd(run_type, run_id, run_name, run_param_id, run_param_name, dtype)
+# outputs = run_command(cmd)
+# latency = float(outputs[-1].split('\n')[0]) # Get last line
+# print(f'{run_name} Latency = {latency}')
