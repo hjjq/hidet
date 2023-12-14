8c1dad1013870d6f5e325f8d9c93da899d02805b
hjjq <hanjie.qiu@mail.utoronto.ca>
2023-12-14 18:13:05 -0500

    .

--- .github/scripts/run_tests.py
+++ .github/scripts/run_tests.py
@@ -62,10 +62,7 @@ if __name__ == '__main__':
         run_dtype_id = run_config['dtype_id']
         run_dtype_name = run_config['dtype_name']
         cmd = get_bench_cmd(run_type, run_id, run_name, run_param_name, run_dtype_name)
-        if False and False:
-            outputs = run_command(cmd)
-        else:
-            outputs = None
+        outputs = run_command(cmd)
         if outputs:
             latency = float(outputs[-1].split('\n')[0]) # Get last line
             run_config['latency'] = latency
