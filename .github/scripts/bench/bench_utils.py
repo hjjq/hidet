974e35605f76f6c990e046819484ec11becece89
hjjq <hanjie.qiu@mail.utoronto.ca>
2023-12-14 13:02:02 -0500

    operator benchmarks

--- .github/scripts/bench/bench_utils.py
+++ .github/scripts/bench/bench_utils.py
@@ -1,18 +1,22 @@
 import os
 import hidet
-import torch
 
-def setup_hidet_flags(dtype):
-    use_fp16 = dtype == 'float16'
-    hidet.torch.dynamo_config.search_space(2)
-    hidet.torch.dynamo_config.use_fp16(use_fp16)
-    hidet.torch.dynamo_config.use_fp16_reduction(use_fp16)
-    hidet.torch.dynamo_config.use_attention(True)
-    hidet.torch.dynamo_config.use_tensor_core(True)
-    hidet.torch.dynamo_config.use_cuda_graph(True)
+def setup_hidet_flags(dtype, dynamo=True):
+    if dynamo:
+        import torch
+        use_fp16 = dtype == 'float16'
+        hidet.torch.dynamo_config.search_space(2)
+        hidet.torch.dynamo_config.use_fp16(use_fp16)
+        hidet.torch.dynamo_config.use_fp16_reduction(use_fp16)
+        hidet.torch.dynamo_config.use_attention(True)
+        hidet.torch.dynamo_config.use_tensor_core(True)
+        hidet.torch.dynamo_config.use_cuda_graph(True)
+    else:
+        hidet.option.search_space(2)
     hidet.option.cache_dir(hidet.option.get_cache_dir() + '/regression')
 
 def bench_torch_model(model, torch_inputs, bench_iters=100, warmup_iters=10):
+    import torch
     for _ in range(warmup_iters):
         torch_out = model(*torch_inputs)
     torch.cuda.empty_cache()
